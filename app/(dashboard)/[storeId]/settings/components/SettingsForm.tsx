cd00e418c5dec93c405c60aacbd32af1aca5cbf1
Leprekus <leprekus@yahoo.com>
2023-09-03 02:08:12 -0700

    implement store settings

---
+++ app/(dashboard)/[storeId]/settings/components/SettingsForm.tsx
@@ -0,0 +1,95 @@
+'use client'
+import Heading from '@/components/ui/Heading';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Store } from '@prisma/client';
+import { Trash } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod'
+import { useState } from 'react';
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+interface SettingsFormProps { store: Store }
+
+const FormSchema = z.object({
+    name: z.string().min(1)
+})
+
+type SettingsFormValues = z.infer<typeof FormSchema>
+
+export default function SettingsForm({ store }: SettingsFormProps) {
+  
+    const [open, setOpen] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm({
+        resolver: zodResolver(FormSchema),
+        defaultValues: store
+    })
+
+    const onSubmit = async (data: SettingsFormValues) => {
+         
+        console.log(data)
+    }
+
+    return (
+    <>
+    <div className='flex items-center justify-between'>
+        <Heading 
+            title='Settings'
+            description='Manage store preferences'
+        />
+        <Button 
+            className='flex items-center justify-between gap-2'
+            variant='destructive'
+            disabled={false}
+            onClick={() => {}}
+        >
+            <Trash className='h-4 w-4'/>
+            Delete
+        </Button>
+    </div>
+    <Separator/>
+    <Form {...form}>
+        <form 
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8 w-full'
+        >
+            <div className='grid grid-cols-3 gap-8'>
+                <FormField
+                    control={form.control}
+                    name='name'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input 
+                                    disabled={isLoading} 
+                                    placeholder='Store name'
+                                    { ...field }
+                                    />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <Button 
+                disabled={isLoading} 
+                type='submit'>
+                Save Changes
+            </Button>
+        </form>
+    </Form>
+    </>
+  )
+}
--- app/(dashboard)/[storeId]/settings/page.tsx
+++ app/(dashboard)/[storeId]/settings/page.tsx
@@ -1,14 +1,26 @@
 import { isAuthed } from '@/lib/helpers';
+import prismadb from '@/lib/prismadb';
+import { redirect } from 'next/navigation';
+import SettingsForm from './components/SettingsForm';
 
 interface SettingsPageProps {
     params: { storeId: string; }
 }
-export default function SettingsPage({ params }: SettingsPageProps) {
+export default async function SettingsPage({ params }: SettingsPageProps) {
     
     const userId = isAuthed()
     
+    const store = await prismadb.store.findFirst({
+        where: { id: params.storeId, userId }
+    })
+
+    if(!store) return redirect('/')
 
     return (
-    <div>page</div>
+    <div className='flex-co'>
+        <div className='flex-1 space-y-4 p-8 pt-6'>
+            <SettingsForm store={store}/>
+        </div>
+    </div>
   )
 }
